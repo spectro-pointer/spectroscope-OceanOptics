7f24b5ec9b6cc3cfd730edd9f11468e6f109a018
Mauro Lacy <mauro@lacy.com.ar>
2016-07-31 23:40:39 +0000

    Add button_event for reference

---
+++ spectrums/button_event.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO, time, os, subprocess
+
+#from detector import Detector
+
+ip = '192.168.2.154'
+#det = Detector(ip)
+
+# Use the Broadcom SOC Pin numbers
+# Setup the Pin with Internal pullups enabled and PIN in reading mode.
+GPIO.setmode(GPIO.BCM)
+gpio_start = 4
+gpio_stop  = 5
+GPIO.setup(gpio_start, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(gpio_stop , GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+# Our function on what to do when the button is pressed
+def Start(channel):
+#	subprocess.call(['spectrometer.py'])
+    print("start")
+#    det.start()
+
+def Stop(channel):
+#	subprocess.call(['spectrometer.py'])
+    print("stop")
+#    det.stop()
+
+# Add our function to execute when the button pressed event happens
+GPIO.add_event_detect(gpio_start, GPIO.FALLING, callback = Start)
+GPIO.add_event_detect(gpio_stop , GPIO.FALLING, callback = Stop )
+
+# Now wait!
+while True:
+    time.sleep(1)
