da2c12093e25a0be6eba595e5015519139c14322
Masoud <masoud@chapar.local>
2016-10-02 00:49:59 +0200

    minor change

--- exp/window_position.py
+++ exp/window_position.py
@@ -1,17 +1,23 @@
 
 import matplotlib
+import os
 # matplotlib.use("Qt4Agg") #
 #matplotlib.use("TkAgg")
 matplotlib.rcParams['toolbar'] = 'None'
+stylefile='../misc/rc/probpro'
+matplotlib.rc_file(os.path.join(os.path.dirname(__file__), stylefile))
 import matplotlib.pyplot as plt
 
 
-plt.figure()
+fig=plt.figure()
 # plt.get_current_fig_manager().window.setGeometry(50,50,200,150) # for QT backend
 plt.get_current_fig_manager().window.wm_geometry("200x400+400+0") # tk backend
 #plt.get_current_fig_manager().window.SetPosition((500, 0)) # wx backend
 
 
 plt.plot([0,1,2,0,1,2]) # draw something
+plt.xlabel('wavelenght (nm)')
+plt.ylabel('amplitude')
+#fig.tight_layout()
 plt.show()
 
