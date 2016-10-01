1a05c9a828388622a64b2ad276fef81f580ac860
Masoud <mpoloton@gmail.com>
2016-10-01 19:23:40 +0200

    added scripts for testing matplotlib backends

---
+++ exp/plot_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import matplotlib
+#list backends 
+matplotlib.rcsetup.interactive_bk
+matplotlib.rcsetup.non_interactive_bk
+matplotlib.rcsetup.all_backends
